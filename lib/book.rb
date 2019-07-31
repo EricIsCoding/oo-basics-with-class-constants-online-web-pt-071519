b65c19404e8329226dfaf2df8fb332658889bc86
Eric J Wilhite <ericjwilhite@gmail.com>
2019-07-31 16:53:58 +0000

    Automatically backed up by Learn

--- lib/book.rb
+++ lib/book.rb
@@ -1,9 +1,12 @@
 class Book
   attr_accessor :author, :page_count, :genre
   attr_reader :title
-
+  
+  @@Genres = []
+  
   def initialize(title)
     @title = title
+    @@Genres << genre
   end
 
   def turn_page
